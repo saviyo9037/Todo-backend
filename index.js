b3bc6b91fd61f58d0cc5d514f043445e9ba15890
Saviyo George <saviyogeorge123@gmail.com>
2025-10-17 11:16:25 +0530

    backend commit15

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const connectDB = () => {
 connectDB();
 
 var corsOptions = {
-  origin: 'http://localhost:5173',
+  origin:  process.env.FRONTEND_URL || 'http://localhost:5173',
   optionsSuccessStatus: 200
 }
 
